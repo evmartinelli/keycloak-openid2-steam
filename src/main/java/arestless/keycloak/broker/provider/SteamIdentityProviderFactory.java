427e5ace8ba02266a2dd1550d30fc07159aa0744
Evandro Martinelli <martinelli.evandro@gmail.com>
2023-11-06 20:55:10 -0300

    Update SteamIdentityProviderFactory.java

--- src/main/java/arestless/keycloak/broker/provider/SteamIdentityProviderFactory.java
+++ src/main/java/arestless/keycloak/broker/provider/SteamIdentityProviderFactory.java
@@ -48,4 +48,8 @@ public class SteamIdentityProviderFactory implements SocialIdentityProviderFacto
     public String getId() {
         return "steam";
     }
+    
+    @Override
+    public void createConfig() {
+    }
 }
